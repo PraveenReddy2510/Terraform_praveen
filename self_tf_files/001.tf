1cdba40927752b6cb12308d0d4575e77637003d7
PraveenReddy2510 <127923130+PraveenReddy2510@users.noreply.github.com>
2024-02-16 15:47:35 +0530

    Update 001.tf

--- self_tf_files/001.tf
+++ self_tf_files/001.tf
@@ -1,4 +1,4 @@
 resource "aws_instance" "praveen-ec2" {
-  ami           = "ami-0c94855ba95c286c99"
+  ami           = "ami-0c94855ba95c286c99" # ami-0c7217cdde317cfec
   instance_type = "t2.micro"
 }
