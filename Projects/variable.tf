4f1595d1cda92199b62d3883919e2c03354d7178
Praveen <rpraveen4712@gmail.com>
2024-09-02 17:32:46 +0530

    updated VPC and variable file

--- Projects/variable.tf
+++ Projects/variable.tf
@@ -1,3 +1,68 @@
 variable "instance_type" {
-  type = "t2.medium"
+  type    = string
+  default = "t2.medium"
+}
+
+variable "availability_zones" {
+  description = "A list of availability zones"
+  type        = map(string)
+  default = {
+    az1 = "us-east-1a"
+    az2 = "us-east-1b"
+  }
+}
+
+variable "vpc_cidr" {
+  type    = list(string)
+  default = ["10.0.0.0/16"]
+}
+
+variable "public_subnet_cidr" {
+  type = map(object({
+    cidr_block = string
+    az         = string
+  }))
+  default = {
+    pb_subnet_1 = {
+      cidr_block = "10.0.1.0/24"
+      az         = "us-east-1a"
+    }
+    pb_subnet_2 = {
+      cidr_block = "10.0.2.0/24"
+      az         = "us-east-1b"
+    }
+  }
+}
+
+variable "private_subnet_cidr" {
+  type = map(object({
+    cidr_block = string
+    az         = string
+  }))
+  default = {
+    pt_subnet_1 = {
+      cidr_block = "10.0.3.0/24"
+      az         = "us-east-1a"
+    }
+    pt_subnet_2 = {
+      cidr_block = "10.0.4.0/24"
+      az         = "us-east-1a"
+    }
+    pt_subnet_3 = {
+      cidr_block = "10.0.5.0/24"
+      az         = "us-east-1a"
+    }
+    pt_subnet_4 = {
+      cidr_block = "10.0.6.0/24"
+      az         = "us-east-1b"
+    }
+    pt_subnet_5 = {
+      cidr_block = "10.0.7.0/24"
+      az         = "us-east-1b"
+    }
+    pt_subnet_6 = {
+      cidr_block = "10.0.8.0/24"
+      az         = "us-east-1b"
+    }
+  }
 }
--- Projects/vpc.tf
+++ Projects/vpc.tf
@@ -1,45 +1,88 @@
 provider "aws" {
-    access_key = ""
-    secret_key = ""
-    region = "us-east-1"
+  profile = "default"
 }
 
 locals {
-    praveen = "praveen"
+  praveen = "praveen"
 }
 
 resource "aws_vpc" "praveen_vpc" {
-    cidr_block = "10.0.0.0/16"
-    tags = {
-        Name = "${local.praveen}_vpc"
-    }
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "${local.praveen}_vpc"
+  }
 }
 
 resource "aws_subnet" "praveen_public_subnet" {
-    vpc_id = aws_vpc.praveen_vpc
-    cidr_block = "10.0.1.0/24"
-    tags = {
-        Name = "${local.praveen}_public_subnet"
-    }
+  vpc_id            = aws_vpc.praveen_vpc.id
+  for_each          = var.public_subnet_cidr
+  cidr_block        = each.value.cidr_block
+  availability_zone = each.value.az
+  tags = {
+    Name = each.key
+  }
 }
 
 resource "aws_subnet" "praveen_private_subnet" {
-    vpc_id = aws_vpc.praveen_vpc
-    cidr_block = "10.0.3.0/24"
-    tags = {
-        Name = "${local.praveen}_private_subnet"
-    }
+  for_each          = var.private_subnet_cidr
+  vpc_id            = aws_vpc.praveen_vpc.id
+  cidr_block        = each.value.cidr_block
+  availability_zone = each.value.az
+
+  tags = {
+    Name = each.key
+  }
+}
+
+resource "aws_internet_gateway" "praveen-IGW" {
+  vpc_id = aws_vpc.praveen_vpc.id
+
+  tags = {
+    Name = "${local.praveen}_IGW"
+  }
+}
+
+resource "aws_route_table" "praveen-rt-public" {
+  vpc_id = aws_vpc.praveen_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.praveen-IGW.id
+  }
+
+  tags = {
+    Name = "public_rt_${local.praveen}"
+  }
+}
+
+resource "aws_route_table_association" "praveen-public-rt-association" {
+  for_each       = aws_subnet.praveen_public_subnet
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.praveen-rt-public.id
+}
+
+resource "aws_route_table" "praveen-rt-private" {
+  vpc_id = aws_vpc.praveen_vpc.id
+  tags = {
+    Name = "private_rt_${local.praveen}"
+  }
+}
+
+resource "aws_route_table_association" "praveen-private-rt-association" {
+  for_each       = aws_subnet.praveen_private_subnet
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.praveen-rt-private.id
 }
 
 resource "aws_instance" "praveen_instance" {
-    ami = ""
-    instance_type = var.instance_type
-    count = 1
-    tags = {
-        Name = "${local.praveen}_ec2"
-    }
+  ami                         = "ami-04a81a99f5ec58529"
+  subnet_id                   = aws_subnet.praveen_public_subnet["pb_subnet_1"].id
+  instance_type               = var.instance_type
+  associate_public_ip_address = true
+  tags = {
+    Name = "${local.praveen}_ec2"
+  }
 }
 
 output "ec2_instance_public_ip" {
-    value = aws_instance.praveen_instance.public_ip
+  value = aws_instance.praveen_instance.public_ip
 }
